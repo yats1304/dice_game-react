82a5b1d76907b0707a70702fe4fbefb93dac4071
yats1304 <chaubaly@gmail.com>
2025-08-14 20:51:23 +0530

    Home page added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import StartGame from "./assets/components/StartGame";
+
+function App() {
+
+  return (
+    <>
+      <StartGame/>
+    </>
+  )
+}
+ 
+export default App;
